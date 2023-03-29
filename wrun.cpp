215efcba621e7bfdfb7b5ae81b07d6d28720bcb5
Marcus Ramse <marcus@ramse.se>
2023-03-29 17:21:59 +0200

    C++ize

--- wrun.cpp
+++ wrun.cpp
@@ -5,16 +5,15 @@
 #include <algorithm>
 #include <filesystem>
 #include <future>
+#include <fstream>
+#include <utility>
+#include <iostream>
 
 #include <cassert> // TODO remove
-#include <cstring> // TODO remove
 extern "C" { // TODO remove
 #include <windows.h>
 #include <shlwapi.h>
 }
-
-#define u16 uint16_t
-#define u32 uint32_t
 
 namespace fs = std::filesystem;
 
@@ -28,17 +27,10 @@ namespace fs = std::filesystem;
 
 const std::set<std::string> INDEX_EXTS = { ".exe", ".lnk", ".bat", ".cmd", ".com", ".url" };
 
-enum IndexSource {
-	WRENT_START,
-	WRENT_PATH,
-	WRENT_CUSTOM,
-};
-
-struct Entry {
-	size_t historyScore;
-	std::u8string title;
-	fs::path path; // lexically_normalized
-	IndexSource source;
+struct IEntry {
+	virtual const std::u8string& getTitle() const = 0;
+	virtual int32_t getScore() const = 0;
+	virtual void run() = 0;
 };
 
 struct PendingMenuProcess {
@@ -54,37 +46,51 @@ struct DaemonData {
 	std::future<void> menuFuture;
 };
 
-std::vector<Entry> gEntries;
-std::map<fs::path, size_t> gHistory; // path is lexically_normalized
+std::vector<IEntry*> gEntries;
+std::map<fs::path, int32_t> gHistory; // path is lexically_normalized
 
-void indexAppend(const fs::path& path, IndexSource source)
-{
-	const u32 historyScore = gHistory.contains(path) ? gHistory[path] : 0;
-	gEntries.push_back(Entry{
-		.historyScore = historyScore,
-		.title = path.filename().u8string(),
-		.path = path.lexically_normal(),
-		.source = source,
-	});
-}
+struct PathEntry : public IEntry {
+	std::u8string title;
+	fs::path path; // lexically_normalized
+
+	PathEntry(const fs::path& _path) :
+		title(_path.filename().u8string()), path(_path.lexically_normal()) {}
+
+	const std::u8string& getTitle() const override 
+	{
+		return title;
+	}
+
+	int32_t getScore() const override 
+	{
+		return gHistory.contains(path) ? gHistory[path] : 0;
+	}
+
+	void run() override 
+	{
+		// Run & update history
+		ShellExecuteW(0, L"open", L"explorer", path.c_str(), 0, SW_SHOW);
+		gHistory[path] = getScore() + 1;
+	}
+};
 
 bool isExecutable(const fs::path& path) {
 	return INDEX_EXTS.contains(path.extension().string());
 }
 
-void indexDir(const fs::path& path, IndexSource source, bool recurse = false)
+void indexDir(const fs::path& path, bool recurse = false)
 {
 	try {
 		if (recurse) {
 			for (const auto& entry : fs::recursive_directory_iterator(path)) {
 				if (isExecutable(entry.path())) {
-					indexAppend(entry.path(), source);
+					gEntries.push_back(new PathEntry(entry.path()));
 				}
 			}
 		} else {
 			for (const auto& entry : fs::directory_iterator(path)) {
 				if (isExecutable(entry.path())) {
-					indexAppend(entry.path(), source);
+					gEntries.push_back(new PathEntry(entry.path()));
 				}
 			}
 		}
@@ -95,8 +101,8 @@ void indexDir(const fs::path& path, IndexSource source, bool recurse = false)
 
 void indexStartMenu()
 {
-	indexDir(fs::path(std::getenv("AppData")).append("Microsoft/Windows/Start Menu/Programs"), WRENT_START, true);
-	indexDir(fs::path(std::getenv("ProgramData")).append("Microsoft/Windows/Start Menu/Programs"), WRENT_START, true);
+	indexDir(fs::path(std::getenv("AppData")).append("Microsoft/Windows/Start Menu/Programs"), true);
+	indexDir(fs::path(std::getenv("ProgramData")).append("Microsoft/Windows/Start Menu/Programs"), true);
 }
 
 void indexEnvPath()
@@ -104,62 +110,49 @@ void indexEnvPath()
 	const auto pathEnv = std::string(std::getenv("PATH"));
 	size_t start = 0, end;
 	while ((end = pathEnv.find(";", start)) != std::string::npos) {
-		indexDir(pathEnv.substr(start, end - start), WRENT_PATH);
+		indexDir(pathEnv.substr(start, end - start));
 		start = end + 1;
 	}
 }
 
-void indexCustomPath(const fs::path& path)
+void loadHistory(const fs::path& histFilePath)
 {
-	indexDir(path, WRENT_CUSTOM);
-}
-
-void loadHistory(const fs::path& path)
-{
-	FILE *f = fopen(path.lexically_normal().string().c_str(), "rb"); // TODO: use C++
-	if (f) {
-		u16 pathBinLen;
-		u32 historyScore;
-		while (fread(&historyScore, 4, 1, f) == 1) {
-			assert(fread(&pathBinLen, 2, 1, f) == 1);
-			assert(pathBinLen && pathBinLen < 10000);
-			std::u8string path;
-			path.resize(pathBinLen);
-			assert(fread(path.data(), pathBinLen, 1, f) == 1);
-			gHistory[fs::path(path).lexically_normal()] = historyScore;
-		}
-		fclose(f);
+	std::ifstream file(histFilePath, std::ifstream::binary);
+	while (file.good()) {
+		uint32_t historyScore = 0;
+		uint16_t pathBinLen = 0;
+		std::u8string path;
+		file.read((char*)&historyScore, 4);
+		if (!historyScore) continue;
+		file.read((char*)&pathBinLen, 2);
+		assert(pathBinLen && pathBinLen < 10000);
+		path.resize(pathBinLen);
+		file.read((char*)path.data(), pathBinLen);
+		gHistory[fs::path(path).lexically_normal()] = historyScore;
 	}
 }
 
-void saveHistory(const fs::path& path)
+void saveHistory(const fs::path& histFilePath)
 {
-	FILE *f = fopen(path.lexically_normal().string().c_str(), "wb"); // TODO: use C++
-	assert(f);
+	std::ofstream file(histFilePath, std::ofstream::binary | std::ofstream::trunc);
 	for (auto& entry : gHistory) {
-		// History is stored as: <launch count:u32><path byte len:u16><path bytes>
-		// TODO: better format
-		const u32 historyScore = (size_t)entry.second;
-		assert(fwrite(&historyScore, 4, 1, f) == 1);
+		const auto historyScore = (uint32_t)entry.second;
 		const auto pathStr = entry.first.u8string();
-		const u16 pathBinLen = pathStr.length();
-		assert(fwrite(&pathBinLen, 2, 1, f) == 1);
-		assert(fwrite(pathStr.c_str(), pathBinLen, 1, f) == 1);
+		const auto pathBinLen = (uint16_t)pathStr.length();
+		if (!historyScore) continue;
+		file.write((const char*)&historyScore, 4);
+		file.write((const char*)&pathBinLen, 2);
+		file.write((const char*)pathStr.data(), pathBinLen);
 	}
-	fclose(f);
 }
 
 void organizeIndex()
 {
-	std::sort(gEntries.begin(), gEntries.end(), [](const Entry& a, const Entry& b) -> bool {
-		if (a.historyScore == b.historyScore) {
-			if (a.source == b.source) {
-				return a.title < b.title;
-			} else {
-				return a.source < b.source;
-			}
+	std::sort(gEntries.begin(), gEntries.end(), [](const IEntry* a, const IEntry* b) -> bool {
+		if (a->getScore() == b->getScore()) {
+			return a->getTitle() < b->getTitle();
 		} else {
-			return a.historyScore > b.historyScore;
+			return a->getScore() > b->getScore();
 		}
 	});
 }
@@ -168,7 +161,6 @@ void organizeIndex()
 // This makes it so that showing the menu is essentially instant
 PendingMenuProcess launchMenu(char *menuCmd)
 {
-	// TODO: switch to C++ methods
 	// Create pipes for menu
 	SECURITY_ATTRIBUTES sa = {
 		.nLength = sizeof(SECURITY_ATTRIBUTES),
@@ -194,8 +186,8 @@ PendingMenuProcess launchMenu(char *menuCmd)
 	CloseHandle(stdoutW);
 
 	// Send menu to stdin
-	for (Entry& entry : gEntries) {
-		WriteFile(stdinW, entry.title.c_str(), entry.title.length(), NULL, NULL);
+	for (auto& entry : gEntries) {
+		WriteFile(stdinW, entry->getTitle().c_str(), entry->getTitle().length(), NULL, NULL);
 		WriteFile(stdinW, "\n", 1, NULL, NULL);
 	}
 
@@ -231,18 +223,11 @@ void showMenu(PendingMenuProcess menu)
 	if (!StrToIntExA(out, STIF_SUPPORT_HEX, &choice) || choice < 0) {
 		fprintf(stderr, "no choice\n");
 		return;
-	} else if ((u32)choice >= gEntries.size()) {
+	} else if ((size_t)choice >= gEntries.size()) {
 		fprintf(stderr, "choice out of range\n");
 		return;
 	}
-	Entry& chosen = gEntries[choice];
-
-	// Open choice
-	ShellExecuteW(0, L"open", L"explorer", chosen.path.c_str(), 0, SW_SHOW);
-
-	// Put in history
-	chosen.historyScore++;
-	gHistory[chosen.path.c_str()] = chosen.historyScore;
+	gEntries[choice]->run();
 }
 
 void runDaemon(DaemonData *dd)
@@ -299,12 +284,12 @@ void usage()
 	// TODO: flag to enable sub-actions (aside from launch), stuff like "Open Directory" and "Remove from History"
 	fprintf(stderr, "wrun <OPTIONS>\n"
 		"USAGE:\n"
-		"\t-menu <menu cmd>  menu command to invoke (required)\n"
-		"\t-daemonize        run in background\n"
-		"\t-Nhistory         don't load or save history\n"
-		"\t-Nstart           don't index start menu\n"
-		"\t-Npath            don't index PATH\n"
-		"\t+index <path>     index a custom path\n"
+		"\t--menu <menu cmd>  menu command to invoke (required)\n"
+		"\t--daemonize        run in background\n"
+		"\t--no-history       don't load or save history\n"
+		"\t--no-start         don't index start menu\n"
+		"\t--no-path          don't index PATH\n"
+		"\t--index <path>     index one or more custom paths\n"
 		"DAEMON:\n"
 		"\tThe -daemonize flag will put wrun into the background\n"
 		"\tand show the menu on a WM_USER (0x400) window message.\n"
@@ -324,19 +309,19 @@ int main(int argc, char **argv)
 	std::vector<std::string> customPaths;
 
 	for (int i = 1; i < argc; i++) {
-		if (!strcmp(argv[i], "-daemonize")) {
+		if (!strcmp(argv[i], "--daemonize")) {
 			daemonize = true;
-		} else if (!strcmp(argv[i], "-Nhistory")) {
+		} else if (!strcmp(argv[i], "--no-history")) {
 			useHistory = false;
-		} else if (!strcmp(argv[i], "-Nstart")) {
+		} else if (!strcmp(argv[i], "--no-start")) {
 			doIndexStartMenu = false;
-		} else if (!strcmp(argv[i], "-Npath")) {
+		} else if (!strcmp(argv[i], "--no-path")) {
 			doIndexEnvPath = false;
 		} else if (i + 1 == argc) {
 			usage();
-		} else if (!strcmp(argv[i], "-menu")) {
+		} else if (!strcmp(argv[i], "--menu")) {
 			menuCmd = argv[++i];
-		} else if (!strcmp(argv[i], "+index")) {
+		} else if (!strcmp(argv[i], "--index")) {
 			customPaths.push_back(argv[++i]);
 		} else {
 			usage();
@@ -353,10 +338,10 @@ int main(int argc, char **argv)
 	if (doIndexStartMenu) indexStartMenu();
 	if (doIndexEnvPath) indexEnvPath();
 	for (size_t i = 0; i < customPaths.size(); i++) {
-		indexCustomPath(customPaths[i]);
+		indexDir(customPaths[i]);
 	}
 	organizeIndex();
-	
+
 	if (daemonize) {
 		runDaemon(new DaemonData{
 			.menuCmd = menuCmd,
